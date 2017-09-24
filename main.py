06c695fc93d56fc937a28951f7c5eb67836399dd
K Mae Petrin <kmaepetrin@gmail.com>
2017-09-24 18:05:14 -0500

    web caesar working UP TO textformat

---
+++ main.py
@@ -0,0 +1,57 @@
+from flask import Flask, request, redirect, render_template
+from caesar import rotate_string
+import cgi
+import os
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+form = """<!DOCTYPE html>
+<html>
+    <head>
+        <title>Caesar Cipher</title>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+        </style>
+    </head>
+    <body>
+    <form method='POST'>
+        <label>Rotate by:
+            <input type="text" name="rot" value="0"/>
+        </label>
+        <textarea name="text">
+        </textarea>
+        <button name="Submit query">Submit Query</button>
+    </form>
+    </body>
+</html
+"""
+
+@app.route('/', methods=['POST'])
+def encrypt():
+    text = request.form["text"]
+    rot_str = request.form["rot"]
+    rot = int(rot_str)
+
+    encrypted = rotate_string(text, rot)
+
+    return encrypted
+
+@app.route('/')
+def index():
+
+    return form
+
+app.run()
